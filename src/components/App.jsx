1824bfbdeb443885463046e732cef4a2b3b63cd2
Fernando Gómez <fernandogomezrecio@gmail.com>
2024-03-07 17:40:05 +0100

    Creando componentes y rutas

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import MyRoutes from '../router/MyRoutes'
+import './App.css'
+import Curriculum from './Curriculum'
+
+const App = () => {
+    return (
+        <>
+            <MyRoutes />
+        </>
+    )
+}
+
+export default App
---
+++ src/router/MyRoutes.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
+import Home from '../components/Home'
+import Portfolio from '../components/Portfolio'
+import Services from '../components/Services'
+import Curriculum from '../components/Curriculum'
+import Contact from '../components/Contact'
+
+const MyRoutes = () => {
+    return (
+        <BrowserRouter>
+            {/* HEADER Y NAVEGACION */}
+
+            {/* CONTENIDO PRINCIPAL */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/inicio" element={<Home />} />
+                <Route path="/portafolio" element={<Portfolio />} />
+                <Route path="/servicios" element={<Services />} />
+                <Route path="/curriculum" element={<Curriculum />} />
+                <Route path="/contacto" element={<Contact />} />
+            </Routes>
+            {/* FOOTER */}
+        </BrowserRouter>
+    )
+}
+
+export default MyRoutes
